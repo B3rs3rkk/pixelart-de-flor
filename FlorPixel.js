2a88205f473eb3c51d7e7d0382fea445fb2cc68b
B3rs3rkk <diegotata2006@gmail.com>
2024-08-18 12:38:52 -0600

    perf: se mejoro mucho el codigo y tambien el diseño

---
+++ FlorPixel.js
@@ -0,0 +1,249 @@
+
+window.onload=function(){}
+    Canvas = document.getElementById('canvas');
+
+    if(canvas && canvas.getContext){
+        ctx = canvas.getContext("2d");
+            if(ctx){
+
+                ctx.fillStyle = "yellow";
+                ctx.strokeStyle = "#98532c";
+                ctx.lineWidth =5;
+                
+            // x,y,w,h
+            // ctx.fillRect(50,50,100,100)
+            // ctx.strokeRect(50,50,100,100)
+
+            // Cambiar el color de relleno 
+            //ctx.fillStyle= "rgba(39,20,230,0.8)"
+            //ctx.fillRect(100,50,100,100)
+            //ctx.strokeRect(200,50,100,100)
+// -----------------------------------------------------------------------------------------------------    
+//parte uno                 
+                // cuadraditos 1
+                ctx.fillStyle = "#45D16E"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(300,340,65,60)
+
+                // cuadraditos 2
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(235,340,65,60)
+//parte dos  
+          
+                  // rectangulo 3
+                ctx.fillStyle = "#45D16E"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(300,400,130,60)
+
+//parte tres 
+                 // rectangulo 4
+                ctx.fillStyle = "#45D16E"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(300,460,130,60)
+
+//parte cuatro 
+                // cuadrado 5
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(300,520,65,60)
+
+                 // cuadrado 6
+                 ctx.fillStyle = "#45D16E"
+                 ctx.strokeStyle = "98532c"
+                 ctx.lineWidth =    5
+                 ctx.fillRect(365,520,65,60)
+
+                 // cuadrado 7
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(430,520,65,60)
+                
+//parte cinco 
+                //rectangulo 8
+                 ctx.fillStyle = "#45D16E"
+                 ctx.strokeStyle = "98532c"
+                 ctx.lineWidth =    5
+                 ctx.fillRect(365,580,260,60)
+
+                // cuadrado 9
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(625,580,65,60)
+//parte seis 
+                //cuadrado 10
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(365,640,65,60) 
+                
+                //rectangulo 11
+                ctx.fillStyle = "#45D16E"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(430,640,130,60) 
+
+                //cuadrado 12
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(560,640,65,60) 
+//parte siete  
+                //cuadrado 13 
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(430,700,65,60) 
+
+                //cuadrado 14 
+                ctx.fillStyle = "#45D16E"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(495,700,65,60) 
+
+                //cuadrado 15 
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(560,700,65,60) 
+//parte ocho  
+                //cuadrado 16 
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(495,760,65,60)   
+//parte nueve              
+                //cuadrado 17
+                ctx.fillStyle = "#45D16E"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(495,520,130,60)  
+                
+                //cuadrado 18 
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(625,520,65,60) 
+//parte dies  
+                //rectangulo 19
+                ctx.fillStyle = "#45D16E"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(495,460,195,60)  
+                
+                //cuadrado 20 
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(690,460,65,60) 
+//parte once  
+                //cuadrado 21 
+                ctx.fillStyle = "#45D16E"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(495,400,65,60)
+
+                //cuadrado 22
+                ctx.fillStyle = "#45D16E"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(625,400,65,60)
+
+                //cuadrado 23 
+                ctx.fillStyle = "#0D854D"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(690,400,65,60)
+//parte doce  
+                 //cuadrado 24 
+                 ctx.fillStyle = "#45D16E"
+                 ctx.strokeStyle = "98532c"
+                 ctx.lineWidth =    5
+                 ctx.fillRect(625,340,65,60)
+//parte trece  
+                // cuadrado 25 
+                ctx.fillStyle = "#0D854D"
+                 ctx.strokeStyle = "98532c"
+                 ctx.lineWidth =    5
+                 ctx.fillRect(495,340,65,60)
+//parte catorce 
+                // cuadrado 26 
+                ctx.fillStyle = "#FF3E88"
+                 ctx.strokeStyle = "98532c"
+                 ctx.lineWidth =    5
+                 ctx.fillRect(495,280,65,60)
+//parte quince 
+                // cuadrado 27
+                ctx.fillStyle = "#FF3E88"
+                 ctx.strokeStyle = "98532c"
+                 ctx.lineWidth =    5
+                 ctx.fillRect(430,220,65,60)
+
+                 // cuadrado 28 
+                ctx.fillStyle = "#F06FA0"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(495,220,65,60)
+
+                // cuadrado 29 
+                ctx.fillStyle = "#FF3E88"
+                 ctx.strokeStyle = "98532c"
+                 ctx.lineWidth =    5
+                 ctx.fillRect(560,220,65,60)
+//parte desiseis  
+                // cuadrado 30 
+                ctx.fillStyle = "#F06FA0"
+                 ctx.strokeStyle = "98532c"
+                 ctx.lineWidth =    5
+                 ctx.fillRect(430,160,65,60)
+
+                  // cuadrado 31 
+                ctx.fillStyle = "yellow"
+                ctx.strokeStyle = "98532c"
+                ctx.lineWidth =    5
+                ctx.fillRect(495,160,65,60)
+                 
+                 // cuadrado 32 
+                 ctx.fillStyle = "#F06FA0"
+                  ctx.strokeStyle = "98532c"
+                  ctx.lineWidth =    5
+                  ctx.fillRect(560,160,65,60)
+// parte desisiete
+                    // cuadrado 33 
+                    ctx.fillStyle = "#F06FA0"
+                    ctx.strokeStyle = "98532c"
+                    ctx.lineWidth =    5
+                    ctx.fillRect(430,100,65,60)
+
+                    // cuadrado 34
+                    ctx.fillStyle = "#F06FA0"
+                    ctx.strokeStyle = "98532c"
+                    ctx.lineWidth =    5
+                    ctx.fillRect(560,100,65,60)
+// parte desiocho
+                    // cuadrado 35
+                    ctx.fillStyle = "#F06FA0"
+                    ctx.strokeStyle = "98532c"
+                    ctx.lineWidth =    5
+                    ctx.fillRect(430,40,65,60)
+
+                    // cuadrado 36
+                    ctx.fillStyle = "#F06FA0"
+                    ctx.strokeStyle = "98532c"
+                    ctx.lineWidth =    5
+                    ctx.fillRect(560,40,65,60)
+
+            }else{
+                
+                alert("si funcion la etiqueta FlorPixel.js")
+            }
+        }
+    
+
